22333fc180f8b99545b7f86637abe5253181cdc9
MICHAEL STANDARSKI <michael.standarski@libertymutual.com>
2017-12-07 17:28:00 -0500

    adjusted tweet interval

--- bot.js
+++ bot.js
@@ -48,7 +48,7 @@ const gReplacements = [  // I removed the regular expressions tokens. They are p
     {find: 'pursuit of happiness', replace: 'Pursuit of Poontang'},
     {find: 'Republican', replace: 'Gnome'},
     {find: 'Democrat', replace: 'Leprechaun'},
-    
+
     {find: 'witnesses', replace: 'these dudes I know'},
     {find: 'allegedly', replace: 'kinda probably'},
     {find: 'new study', replace: 'tumblr post'},
@@ -260,7 +260,7 @@ const makeTweet = function () {
 
 
 
-setInterval(makeTweet, 1000 * 10); // not sure why the original had a setTimeout. Seemed like interval was all I needed.
+setInterval(makeTweet, 1000 * 60 * 20); // not sure why the original had a setTimeout. Seemed like interval was all I needed.
 
 // Now there is one thing left and that is when to run updateHeadlines. You can do it at least 2 ways
 // 1) inside makeTweet, just call updateRandomHeadlines
@@ -268,4 +268,4 @@ setInterval(makeTweet, 1000 * 10); // not sure why the original had a setTimeout
 // The hard part is getting headlines is async so you can't be sure when or if it will come back
 // So you can just wait or you can wrap the callbacks or return a promise, bascially you have to decide as the designer of the system
 // What I choose was simply to wait
-setInterval(updateRandomHeadlines, 1000 * 2); // get new headlines every x amount of seconds
+setInterval(updateRandomHeadlines, 1000 * 60); // get new headlines every x amount of seconds
